da4e1e8452cc60491b02689fe7a27a01314e45ac
tedwss <63327530+tedwss@users.noreply.github.com>
2020-07-07 16:29:15 +0800

    2mgame

---
+++ 2m/Assets/Script/HorrorOBJ.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class HorrorOBJ : MonoBehaviour, ITriggerEvent
+{
+    public virtual void TriggerEvent()
+    {
+        print("!");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "玩家") TriggerEvent();
+    }
+}
